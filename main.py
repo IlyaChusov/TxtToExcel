2bac1d7127721b657eb18a1f412f08827715e66c
Chusov Ilya <ilyachusov@gmail.com>
2022-04-15 17:22:10 +0300

    Excel lib added, main funcs tested

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello world')
+try:
+    import xlsxwriter
+except ImportError:
+    import os
+    os.system('pip install xlsxwriter')
+    import xlsxwriter
+
+import glob
+
+
+workbook = xlsxwriter.Workbook('результат.xlsx')
+
+sheet = workbook.add_worksheet()
+
+i = 1
+for file in glob.glob("*.txt"):
+    newfile = open(file)
+    sheet.write('A' + str(i), newfile.read())
+    i = i + 1
+
+workbook.close()
+
+
+
